0eae5bc24d3548b23030eb23b165308e6f7e2d92
SuperBrainBro <superbrainstormers@gmail.com>
2020-03-30 01:36:31 -0700

    few changes for 0.9 but didnt publish

--- NPCs/BossBitzy/BitzyBoss.cs
+++ NPCs/BossBitzy/BitzyBoss.cs
@@ -3,7 +3,7 @@ using Bitzy.Items;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using System;
-
+using Bitzy.Projectiles.BossBitzy;
 using System.IO;
 using Terraria;
 using Terraria.ID;
@@ -28,14 +28,13 @@ namespace Bitzy.NPCs.BossBitzy
 		{
 			npc.aiStyle = 1;
 			npc.stepSpeed = npc.stepSpeed * 2;
-			
 			npc.lifeMax = 2500;
 			npc.damage = 40;
 			npc.defense = 20;
 			npc.knockBackResist = 0.1f;
 			npc.width = 96;
 			npc.height = 84;
-			npc.value = Item.buyPrice(0, 20, 0, 0);
+			npc.value = Item.buyPrice(0, 1, 0, 0);
 			npc.npcSlots = 15f;
 			npc.boss = true;
 			npc.lavaImmune = true;
@@ -43,6 +42,7 @@ namespace Bitzy.NPCs.BossBitzy
 			npc.DeathSound = SoundID.NPCDeath1;
 			music = MusicID.Boss1;
 		}
+
 
 		public override void BossLoot(ref string name, ref int potionType)
 		{
@@ -74,6 +74,58 @@ namespace Bitzy.NPCs.BossBitzy
 			if (Main.expertMode || Main.rand.NextBool())
 			{
 				player.AddBuff(BuffID.Confused, 100, true);
+			}
+		}
+
+		public override void AI()
+		{
+			npc.TargetClosest(true);
+
+			if (npc.life <= npc.lifeMax / 1.5)
+			{
+				npc.ai[0] = 1;
+			}
+
+			if (npc.ai[0] == 1)
+			{
+				npc.ai[2]++;
+				npc.ai[3]++;
+				//if (npc.ai[2] >= 30 + (npc.life / 800))
+				//{
+					//float Speed = 10f;
+					//Vector2 vector8 = new Vector2(npc.position.X + (npc.width / 2), npc.position.Y + (npc.height / 2));
+					//int damage = 70;
+					//int type = ProjectileType<BossBitzyBulletProj>();
+					//float rotation = (float)Math.Atan2(vector8.Y - (Main.player[npc.target].position.Y + (Main.player[npc.target].height * 0.5f)), vector8.X - (Main.player[npc.target].position.X + (Main.player[npc.target].width * 0.5f) - 50));
+					//int num54 = Projectile.NewProjectile(vector8.X, vector8.Y, (float)((Math.Cos(rotation) * Speed) * -1), (float)((Math.Sin(rotation) * Speed) * -1), type, damage, 0f, 0);
+					//rotation = (float)Math.Atan2(vector8.Y - (Main.player[npc.target].position.Y + (Main.player[npc.target].height * 0.5f) + 50), vector8.X - (Main.player[npc.target].position.X + (Main.player[npc.target].width * 0.5f) + 50));
+					//Projectile.NewProjectile(vector8.X, vector8.Y, (float)((Math.Cos(rotation) * Speed) * -1), (float)((Math.Sin(rotation) * Speed) * -1), type, damage, 0f, 0);
+					//npc.ai[2] = 0;
+				//}
+				if (npc.ai[3] >= 25 + (npc.life / 800))
+				{
+					float Speed = 8f;
+					Vector2 vector8 = new Vector2(npc.position.X + (npc.width / 2), npc.position.Y + (npc.height / 2));
+					int damage = 10;
+					int type = ProjectileType<BossBitzyBulletProj>(); //Original is 129
+					float rotation = (float)Math.Atan2(vector8.Y - (Main.player[npc.target].position.Y + (Main.player[npc.target].height * 0.5f)), vector8.X - (Main.player[npc.target].position.X + (Main.player[npc.target].width * 0.5f)));
+					int num54 = Projectile.NewProjectile(vector8.X, vector8.Y, (float)((Math.Cos(rotation) * Speed) * -1), (float)((Math.Sin(rotation) * Speed) * -1), type, damage, 0f, 0);
+					npc.ai[3] = 0;
+				}
+				//if (npc.ai[3] >= 60) //Nebula Laser
+				//{
+				//float Speed = 8f;
+				//Vector2 vector8 = new Vector2(npc.position.X + (npc.width / 2), npc.position.Y + (npc.height / 2));
+				//int damage = 30;
+				//int type = 576;
+				//float rotation = (float)Math.Atan2(vector8.Y - (Main.player[npc.target].position.Y + (Main.player[npc.target].height * 0.5f)), vector8.X - (Main.player[npc.target].position.X + (Main.player[npc.target].width * 0.5f)));
+				//int num54 = Projectile.NewProjectile(vector8.X, vector8.Y, (float)((Math.Cos(rotation) * Speed) * -1), (float)((Math.Sin(rotation) * Speed) * -1), type, damage, 0f, 0);
+				//rotation = (float)Math.Atan2(vector8.Y - (Main.player[npc.target].position.Y + (Main.player[npc.target].height * 0.5f) + 25), vector8.X - (Main.player[npc.target].position.X + (Main.player[npc.target].width * 0.5f) + 25));
+				//Projectile.NewProjectile(vector8.X, vector8.Y, (float)((Math.Cos(rotation) * Speed) * -1), (float)((Math.Sin(rotation) * Speed) * -1), type, damage, 0f, 0);
+				//rotation = (float)Math.Atan2(vector8.Y - (Main.player[npc.target].position.Y + (Main.player[npc.target].height * 0.5f) - 25), vector8.X - (Main.player[npc.target].position.X + (Main.player[npc.target].width * 0.5f) - 25));
+				//Projectile.NewProjectile(vector8.X, vector8.Y, (float)((Math.Cos(rotation) * Speed) * -1), (float)((Math.Sin(rotation) * Speed) * -1), type, damage, 0f, 0);
+				//npc.ai[3] = 0;
+				//}
 			}
 		}
 	}
---
+++ Projectiles/BossBitzy/BossBitzyBulletProj.cs
@@ -0,0 +1,85 @@
+using Bitzy.Dusts;
+using Microsoft.Xna.Framework;
+using System;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using static Terraria.ModLoader.ModContent;
+
+namespace Bitzy.Projectiles.BossBitzy
+{
+	//ported from my tAPI mod because I'm lazy
+	public class BossBitzyBulletProj : ModProjectile
+	{
+		public override void SetStaticDefaults()
+		{
+			ProjectileID.Sets.Homing[projectile.type] = true;
+		}
+
+		public override void SetDefaults()
+		{
+			projectile.width = 8;
+			projectile.height = 8;
+			projectile.alpha = 255;
+			projectile.friendly = false;
+			projectile.hostile = true;
+			projectile.tileCollide = false;
+			projectile.ignoreWater = true;
+			//projectile.ranged = true;
+		}
+
+		public override void AI()
+		{
+			if (projectile.alpha > 70)
+			{
+				projectile.alpha -= 15;
+				if (projectile.alpha < 70)
+				{
+					projectile.alpha = 70;
+				}
+			}
+			if (projectile.localAI[0] == 0f)
+			{
+				AdjustMagnitude(ref projectile.velocity);
+				projectile.localAI[0] = 1f;
+			}
+			Vector2 move = Vector2.Zero;
+			float distance = 400f;
+			bool target = false;
+			for (int k = 0; k < Main.maxPlayers; k++)
+			{
+				if (Main.player[k].active && !Main.player[k].dead)
+				{
+					Vector2 newMove = Main.player[k].Center;
+					float distanceTo = (float)Math.Sqrt(newMove.X * newMove.X + newMove.Y * newMove.Y);
+					if (distanceTo < distance)
+					{
+						move = newMove;
+						distance = distanceTo;
+						target = true;
+					}
+				}
+			}
+			if (target)
+			{
+				AdjustMagnitude(ref move);
+				projectile.velocity = (10 * projectile.velocity + move) / 11f;
+				AdjustMagnitude(ref projectile.velocity);
+			}
+			if (projectile.alpha <= 100)
+			{
+				int dust = Dust.NewDust(projectile.position, projectile.width, projectile.height, DustType<BitzyDust>());
+				Main.dust[dust].velocity /= 2f;
+			}
+		}
+
+		private void AdjustMagnitude(ref Vector2 vector)
+		{
+			float magnitude = (float)Math.Sqrt(vector.X * vector.X + vector.Y * vector.Y);
+			if (magnitude > 6f)
+			{
+				vector *= 6f / magnitude;
+			}
+		}
+	}
+}
